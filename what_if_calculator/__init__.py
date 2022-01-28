20c653275cb9cd262c2c9f55c71cd79d819613d8
Linfeng Li <llinfeng@umich.edu>
2022-01-28 11:38:11 -0500

    Move player-relevant functions to be standalone functions

--- what_if_calculator/__init__.py
+++ what_if_calculator/__init__.py
@@ -26,19 +26,19 @@ class EXP_DemandFunction:
         else:
             return quantity_sold
 
-
+def calculate_profit(price, cost, units_sold, units_produced):
+    return units_sold * price - cost * units_produced
 
 #
 class C(BaseConstants):
     NAME_IN_URL = 'input_calculation'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
-    # Parameters of the dumb demand function
+    # Parameters of the simple demand function
     HIGH_DEMAND_MAX = 100
     HIGH_DEMAND_PRICE_COEF = -1
     LOW_DEMAND_MAX  = 50
     LOW_DEMAND_PRICE_COEF = -2
-
     COST_PER_UNIT = 5
     
 
@@ -48,6 +48,7 @@ class Subsession(BaseSubsession):
 
 def creating_session(subsession: Subsession):
     for p in subsession.get_players():
+        # Initialize the log-collecting tool.
         p.calculator_log = ""
 
 
@@ -55,8 +56,6 @@ def creating_session(subsession: Subsession):
 class Group(BaseGroup):
     pass
 
-def calculate_profit(price, cost, units_sold, units_produced):
-    return units_sold * price - cost * units_produced
 
 class Player(BasePlayer):
     # Note, these are all vars that are round-specific
@@ -67,106 +66,111 @@ class Player(BasePlayer):
     high_demand_status = models.BooleanField()
     calculator_log = models.StringField()
 
-    def random_init_instance():
-        return random.Random(player.participant.label)
-    def check_if_high_demand_status():
-        player_specific_random_instance = player.random_init_instance()
-        # Then, make use of the random instance to decide on the status of demand
-        high_demand_status = player_specific_random_instance.randint(0, 1)   
-        return high_demand_status == 1
 
-    @staticmethod
-    def calc_player_profit(player): 
-        if player.check_if_high_demand_status(player):
-            amount_sold = min(
-                    player.quantity, # Amount subject chose as quantity
-                    EXP_DemandFunction.high_demand(player.price) # Demand amount
-                    ) 
-        else:
-            amount_sold = min(
-                    player.quantity, 
-                    EXP_DemandFunction.low_demand(player.price)
-                    )
+    
 
-        player.profit = calculate_profit(
-                price = player.price,
-                cost = C.COST_PER_UNIT,
-                units_sold = amount_sold,
-                units_produced = player.quantity,
-                )
+def log_player_attempt(player:Player):
+    player.calculator_log += f"p={player.price},q={player.quantity},t={time.time()};"
+    print("Log added")
 
-
-    def calc_high_demand_profit(player):
+def calc_player_profit(player:Player): 
+    if check_if_high_demand_status(player):
         amount_sold = min(
                 player.quantity, # Amount subject chose as quantity
                 EXP_DemandFunction.high_demand(player.price) # Demand amount
                 ) 
-        print(dict(
-                price = player.price,
-                cost = C.COST_PER_UNIT,
-                units_sold = amount_sold,
-                units_produced = player.quantity,
-            )
-            )
-        return calculate_profit(
-                price = player.price,
-                cost = C.COST_PER_UNIT,
-                units_sold = amount_sold,
-                units_produced = player.quantity,
-                )
-
-
-    def calc_low_demand_profit(player):
+    else:
         amount_sold = min(
                 player.quantity, 
                 EXP_DemandFunction.low_demand(player.price)
                 )
-        print(dict(
-                price = player.price,
-                cost = C.COST_PER_UNIT,
-                units_sold = amount_sold,
-                units_produced = player.quantity,
+    player.profit = calculate_profit(
+            price = player.price,
+            cost = C.COST_PER_UNIT,
+            units_sold = amount_sold,
+            units_produced = player.quantity,
             )
+
+def calc_high_demand_profit(player:Player):
+    amount_sold = min(
+            player.quantity, # Amount subject chose as quantity
+            EXP_DemandFunction.high_demand(player.price) # Demand amount
+            ) 
+    print(dict(
+            price = player.price,
+            cost = C.COST_PER_UNIT,
+            units_sold = amount_sold,
+            units_produced = player.quantity,
+        )
+        )
+    return calculate_profit(
+            price = player.price,
+            cost = C.COST_PER_UNIT,
+            units_sold = amount_sold,
+            units_produced = player.quantity,
             )
-        return calculate_profit(
-                price = player.price,
-                cost = C.COST_PER_UNIT,
-                units_sold = amount_sold,
-                units_produced = player.quantity,
-                )
 
-        
 
-    @staticmethod
-    def log_player_attempt(player):
-        player.calculator_log += f"p={player.price},q={player.quantity},t={time.time()};"
-        print("Log added")
+def calc_low_demand_profit(player:Player):
+    amount_sold = min(
+            player.quantity, 
+            EXP_DemandFunction.low_demand(player.price)
+            )
+    print(dict(
+            price = player.price,
+            cost = C.COST_PER_UNIT,
+            units_sold = amount_sold,
+            units_produced = player.quantity,
+        )
+        )
+    return calculate_profit(
+            price = player.price,
+            cost = C.COST_PER_UNIT,
+            units_sold = amount_sold,
+            units_produced = player.quantity,
+            )
 
+
+# For deciding which demand function the subject is assigned to.
+def random_init_instance(player:Player):
+    # Note, we initialize a "fixed" random state for each subject, pertaining to the participant.label
+    return random.Random(player.participant.label)
+def check_if_high_demand_status(player:Player):
+    player_specific_random_instance = random_init_instance()
+    # Then, make use of the random instance to decide on the status of demand
+    high_demand_status = player_specific_random_instance.randint(0, 1)   
+    return high_demand_status == 1
+
+
+
+
+
+
+
+def live_method(player:Player,data):
+    # Upon receiving the data, do the following
+    player.price = data['price_set']
+    player.quantity = data['quantity_set']
+    log_player_attempt(player)
+    return {player.id_in_group:
+            {
+            'high_demand_profit': calc_high_demand_profit(player),
+            'low_demand_profit': calc_low_demand_profit(player)
+            }
+            }
 
 
 
 # PAGES
 class MyPage(Page):
+    live_method = live_method
     form_model = 'player'
     form_fields = ['quantity', 'price']
 
     @staticmethod
     def before_next_page(player):
         # Label the clicktime
-        player.calc_player_profit(player)
-
-    @staticmethod
-    def live_method(player,data):
-        # Upon receiving the data, do the following
-        player.price = data['price_set']
-        player.quantity = data['quantity_set']
-        player.log_player_attempt(player)
-        return {player.id_in_group:
-                {
-                'high_demand_profit': player.calc_high_demand_profit(),
-                'low_demand_profit': player.calc_low_demand_profit()
-                }
-                }
+        calc_player_profit(player)
         
 
     @staticmethod
@@ -178,32 +182,6 @@ class MyPage(Page):
                 LOW_DEMAND_MAX  = 50,
                 LOW_DEMAND_PRICE_COEF = -2,
                 )
-
-
-
-
-def live_method(player: Player, data):
-    # Do the calculation using native Python function
-    # Restricted by the Javascript conditions, we 
-    # if data:
-    #     if is_finished(player):
-    #         return
-
-    # if is_finished(player):
-    #     return {player.id_in_group: dict(is_finished=True)}
-
-
-
-    print("triggering the live_method")
-    return {
-            player.id_in_group:  "something back"
-            # {
-            #     'high_demand_profit': 100,
-            #     'low_demand_profit': 50
-            #     }
-            }
-
-
 
 
 page_sequence = [MyPage]
